0eca73141c05ec8b4f657c3022c6ac7827e47045
SergiOca87 <ocasergi@gmail.com>
2024-06-03 01:27:44 +0200

    first commit

---
+++ inmotionUiLibrary/docs/01-index.md
@@ -0,0 +1,21 @@
+---
+title: inMotion UI Library
+---
+
+# This UI library uses **Atomic Design** Principles.
+
+Atomic Design is a methodology for creating design systems in a hierarchical and systematic way. It breaks down the UI into fundamental building blocks. The main levels in Atomic Design are:
+
+1. **Atoms**: The basic building blocks of a UI, such as buttons, inputs, and labels.
+
+
+2. **Molecules**: Groups of atoms bonded together to form a functional unit, like a search bar (input field + button).
+
+
+3. **Organisms**: Complex components composed of molecules and atoms, such as a header or a form.
+
+
+4. **Templates**: Page-level structures that include groups of organisms arranged in a layout.
+
+
+5. **Pages**: Specific instances of templates, populated with real content.
---
+++ inmotionUiLibrary/fractal.config.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+
+/*
+* Theme
+*/
+const mandelbrot = require('@frctl/mandelbrot');
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'Inmotion Ui Library');
+
+/*
+ * Project Version.
+ */
+fractal.set('project.version', 'v1.0');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(__dirname, 'components'));
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+//fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+fractal.web.set('static.path', __dirname + '/assets');
+
+
+fractal.web.theme(
+    mandelbrot({
+        skin: {
+            name: 'fuchsia',
+            accent: '#ffcd19',
+            complement: '#000',
+            links: '#6f8587',
+        },
+        information: [
+            {
+                label: 'Version',
+                value: require('./package.json').version,
+            },
+            {
+                label: 'Built on',
+                value: new Date(),
+                type: 'time',
+                format: (value) => {
+                    return value.toLocaleDateString('en');
+                },
+            },
+        ],
+    })
+);
